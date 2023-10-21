1d994ecad420268cf624ed5e2cb045bb77020645
Luis Gustavo Santarosa Pinto <bolota_xd@hotmail.com>
2023-10-21 19:28:07 -0300

    Update SetSchemaTrait.php

--- src/SetSchemaTrait.php
+++ src/SetSchemaTrait.php
@@ -4,13 +4,18 @@ namespace GustavoSantarosa\SetSchemaTrait;
 
 trait SetSchemaTrait
 {
-    public function initializeTraits()
+    /**
+     * Initialize the soft deleting trait for an instance.
+     *
+     * @return void
+     */
+    public function initializeSetSchemaTrait()
     {
         $this->setSchema();
     }
 
     public function setSchema()
     {
-        $this->setTable(strtolower(explode('\\', static::class)[3]).'.'.$this->getTable());
+        $this->setTable(strtolower(explode('\\', static::class)[3]) . '.' . $this->getTable());
     }
 }
